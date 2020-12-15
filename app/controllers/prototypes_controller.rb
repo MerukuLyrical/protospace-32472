ead4e6975ab8f126ddf208507f01f16e4b314eb4
MerukuLyrical <merukurip@gmail.com>
2020-12-15 18:44:32 +0900

    ログイン状態の条件分岐

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,13 +1,13 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :authenticate_user!
+  # before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:profile])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:occupation])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:position])
-  end
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:profile])
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:occupation])
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:position])
+  # end
 
   def index
   end
