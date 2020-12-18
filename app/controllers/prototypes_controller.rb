3ddbcf0a09f88168c6cfab4be8c879b2f47a67f7
MerukuLyrical <merukurip@gmail.com>
2020-12-18 20:00:30 +0900

    追加機能実装終了

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: :edit
 
   def index
     @prototypes = Prototype.all
@@ -52,4 +53,9 @@ class PrototypesController < ApplicationController
   params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
+  def move_to_index
+    unless user_signed_in?
+      redirect_to root_path
+    end
+  end
 end
